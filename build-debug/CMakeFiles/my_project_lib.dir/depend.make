492c229abca2f54567c762b3c2c7f2200b03c1b6
Smith35711 <gvkljdf@gmail.com>
2026-07-05 20:11:57 +0800

    chore: remove build artifacts from repo, update gitignore

--- build-debug/CMakeFiles/my_project_lib.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for my_project_lib.
-# This may be replaced when dependencies are built.
--- build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gzq/new_project
-# Build directory: /home/gzq/new_project/build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(all_tests "/home/gzq/new_project/build-debug/test_runner")
-set_tests_properties(all_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/gzq/new_project/CMakeLists.txt;295;add_test;/home/gzq/new_project/CMakeLists.txt;0;")
